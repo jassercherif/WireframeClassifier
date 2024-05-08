ffb2f49cd842a0cf614ae14e8f74d1ee34a26637
jasser cherif <147918952+jassercherif@users.noreply.github.com>
2024-05-08 11:26:13 +0100

    Update WireFrameClassifier.py

--- WireFrameClassifier.py
+++ WireFrameClassifier.py
@@ -16,7 +16,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Appliquer un flou gaussien pour réduire le bruit
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
-# Détecter les contours avec l'algorithme de Canny
+# Detecting edges with Canny's algorithm
 edged = cv2.Canny(blurred, 30, 150)
 
 # Trouver les contours dans l'image
