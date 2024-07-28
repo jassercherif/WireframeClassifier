e2c370232a2390668b03da6b32a19d62d4d28804
jasser cherif <147918952+jassercherif@users.noreply.github.com>
2024-07-28 18:28:36 +0100

    Update WireFrameClassifier.py

--- WireFrameClassifier.py
+++ WireFrameClassifier.py
@@ -1,20 +1,20 @@
 
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-from tensorflow.keras.models import load_model
-
-# Load the trained classification model
-model = load_model('CNN.keras')
-
-# Load the image
-image = cv2.imread('../images/mo4.jpg')
-classes = ['button','imagecercle','imagerectangle','input','logo','text']
-# Convertir l'image en niveaux de gris
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# Appliquer un flou gaussien pour réduire le bruit
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+import matplotlib.pyplot as plt 
+import numpy as np 
+from tensorflow.keras.models import load_model 
+ 
+# Load the trained classification model 
+model = load_model('CNN.keras') 
+ 
+# Load the image 
+image = cv2.imread('../images/mo4.jpg')  
+classes = ['button','imagecercle','imagerectangle','input','logo','text'] 
+# Convertir l'image en niveaux de gris 
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+ 
+# Appliquer un flou gaussien pour réduire le bruit 
+blurred = cv2.GaussianBlur(gray, (5, 5), 0) 
 
 # Detecting edges with Canny's algorithm
 edged = cv2.Canny(blurred, 30, 150)
