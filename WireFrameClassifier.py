99fb539bce4eb5d5030ec40462c5e24bd1ebf1e5
jasser cherif <147918952+jassercherif@users.noreply.github.com>
2024-08-07 11:29:17 +0100

    Update WireFrameClassifier.py

--- WireFrameClassifier.py
+++ WireFrameClassifier.py
@@ -4,33 +4,33 @@ import matplotlib.pyplot as plt
 import numpy as np 
 from tensorflow.keras.models import load_model 
   
-# Load the trained classification model 
+# Load the trained classification model  
 model = load_model('CNN.keras') 
-  
-# Load the image  
-image = cv2.imread('../images/mo4.jpg')   
-classes = ['button','imagecercle','imagerectangle','input','logo','text']  
-# Convertir l'image en niveaux de gris  
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  
    
-# Appliquer un flou gaussien pour réduire le bruit  
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)  
-  
-# Detecting edges with Canny's algorithm 
-edged = cv2.Canny(blurred, 30, 150) 
-  
-# Trouver les contours dans l'image 
-contours, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
-  
-# Pour chaque contour détecté 
-for contour in contours: 
-    # Extract coordinates from bounding box 
-    x, y, w, h = cv2.boundingRect(contour) 
-     
-    # Extraire la région d'intérêt (ROI) de l'image originale
-    roi = image[y:y+h, x:x+w]
-     
-    # Redimensionner la ROI en 224x224
+# Load the image   
+image = cv2.imread('../images/mo4.jpg')    
+classes = ['button','imagecercle','imagerectangle','input','logo','text']   
+# Convertir l'image en niveaux de gris   
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)   
+    
+# Appliquer un flou gaussien pour réduire le bruit   
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)   
+   
+# Detecting edges with Canny's algorithm  
+edged = cv2.Canny(blurred, 30, 150)  
+   
+# Trouver les contours dans l'image  
+contours, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  
+   
+# Pour chaque contour détecté  
+for contour in contours:  
+    # Extract coordinates from bounding box  
+    x, y, w, h = cv2.boundingRect(contour)  
+      
+    # Extraire la région d'intérêt (ROI) de l'image originale 
+    roi = image[y:y+h, x:x+w] 
+      
+    # Redimensionner la ROI en 224x224 
     roi_resized = cv2.resize(roi, (224, 224))
  
     # Prétraiter la ROI pour l'adapter à l'entrée du modèle
