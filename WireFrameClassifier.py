df459cf19126acb03f28eb00abcdf324c2cf7b6a
jasser cherif <147918952+jassercherif@users.noreply.github.com>
2024-09-04 11:10:52 +0100

    Update WireFrameClassifier.py

--- WireFrameClassifier.py
+++ WireFrameClassifier.py
@@ -13,25 +13,25 @@ classes = ['button','imagecercle','imagerectangle','input','logo','text']
 # Convertir l'image en niveaux de gris         
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)         
        
-# Appliquer un flou gaussien pour réduire le bruit         
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)        
+# Appliquer un flou gaussien pour réduire le bruit          
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)         
        
-# Detecting edges with Canny's algorithm         
-edged = cv2.Canny(blurred, 30, 150)       
+# Detecting edges with Canny's algorithm          
+edged = cv2.Canny(blurred, 30, 150)        
      
-# Trouver les contours dans l'image       
-contours, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)        
+# Trouver les contours dans l'image        
+contours, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)         
       
-# Pour chaque contour détecté        
-for contour in contours:         
-    # Extract coordinates from bounding box        
-    x, y, w, h = cv2.boundingRect(contour)        
+# Pour chaque contour détecté         
+for contour in contours:          
+    # Extract coordinates from bounding box         
+    x, y, w, h = cv2.boundingRect(contour)         
        
-    # Extraire la région d'intérêt (ROI) de l'image originale     
-    roi = image[y:y+h, x:x+w]      
+    # Extraire la région d'intérêt (ROI) de l'image originale      
+    roi = image[y:y+h, x:x+w]       
       
-    # Redimensionner la ROI en 224x224    
-    roi_resized = cv2.resize(roi, (224, 224))  
+    # Redimensionner la ROI en 224x224     
+    roi_resized = cv2.resize(roi, (224, 224))   
  
     # Prétraiter la ROI pour l'adapter à l'entrée du modèle  
     roi_preprocessed = np.expand_dims(roi_resized, axis=0)  # Ajouter une dimension batch  
